7c0ed74c93db3667f2f50963d0167b2bc17a8932
JackMarx <seriousfools@gmail.com>
2019-08-30 00:04:09 -0500

    secret

---
+++ app/controllers/api/systems_controller.rb
@@ -0,0 +1,35 @@
+class Api::SystemsController < ApplicationController
+
+  def init
+    render 'init.json.jb'
+  end
+
+  def ncu3fn8u
+    if params[:secret] == "nasdf82fe2"
+      open_gate_1
+      render 'ncu3fn8u.json.jb'
+    else
+      redirect_to root_path
+    end
+  end
+
+  def cef3
+    if params[:operation] == "centerfuge" && current_gate >= 1
+      open_gate_2
+      render 'cef3.json.jb'
+    else
+      redirect_to root_path 
+    end
+  end
+
+  def c34ew
+    #request.POST is a list of only body params
+    if request.POST[:keyword] == "budapest" && current_gate >= 2
+      open_gate_3
+      render 'c34ew.json.jb'
+    else
+      render json: {nope: true}
+    end
+  end
+
+end
